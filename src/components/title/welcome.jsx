d550ca8ba2308306972c84ce03bbe0a4e8103482
jondabubeta <jondabubeta@gmail.com>
2025-07-19 23:18:34 -0700

    Updated welcome text to the current implementation

--- src/components/title/welcome.jsx
+++ src/components/title/welcome.jsx
@@ -5,12 +5,12 @@ This interactive terminal lets you explore my work, background, and contact info
 
 Type any of the following commands to get started:
 
-  • help        – List all available commands
-  • about       – Learn more about me and what I do
-  • projects    – View featured projects and case studies
-  • contact     – Get in touch or view my socials
-  • resume      – View my resume
-  • clear       – Clear the terminal
+  • help             – List all available commands
+  • resume           – View my resume (use --full=true for full version)
+  • view about       – Learn more about me and what I do
+  • view projects    – View featured projects and case studies
+  • view contact     – Get in touch or view my socials
+  • clear            – Clear the terminal
 
 Type a command and press [Enter] to execute it.
 `;
