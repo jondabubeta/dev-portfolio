b13f76a9e100074a6041cfad943fa93899478239
jondabubeta <jondabubeta@gmail.com>
2025-10-16 23:58:05 -0700

    Added fix for Education unique key error.

--- src/components/panel/EducationPanel.jsx
+++ src/components/panel/EducationPanel.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import education from "../../data/education.json";
 
+const keyFor = (ed, i) =>
+  [ed.id, ed.institution, ed.degree, ed.field, ed.year, i]
+    .filter(Boolean)
+    .join("::");
+
 export default function EducationPanel() {
   return (
-    <div className="section-container">
-      <h3>Education</h3>
-      <div className="scrollable-table">
-        <div className="panel-table">
-          {education.map((ed) => (
-            <div key={ed.institution} className="panel-row">
-              <span className="panel-label">{ed.degree}</span>
-              <span className="panel-meta">{ed.field}</span>
-              <span className="panel-extra">{ed.year}</span>
-            </div>
-          ))}
+      <div className="section-container education-section">
+        <h3>Education</h3>
+
+        <div className="scrollable-table">
+          <div className="panel-table education-table">
+            {education.map((ed, i) => (
+              <div className="panel-row" key={keyFor(ed, i)}>
+                <span className="panel-label">{ed.institution}</span>
+                <span className="panel-meta">{ed.degree || ed.field || ""}</span>
+                <span className="panel-extra">{ed.year || ""}</span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
   );
 }
