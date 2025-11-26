a7824f22252b304c80675b7a41c6c9e36d29af3e
jondabubeta <jondabubeta@gmail.com>
2025-11-25 22:03:03 -0800

    Block cursor support

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -187,8 +187,7 @@ const Terminal = forwardRef((props, ref) => {
 
         <div className="input-line">
           <span className="prompt">&gt;&nbsp;</span>
-          <span className="input-text">{input}</span>
-          {focused && showCursor && <span className="block-cursor">█</span>}
+          <span className="input-text">{input}{focused && showCursor && <span className="block-cursor">█</span>}</span>
           <input
             ref={inputRef}
             type="text"
